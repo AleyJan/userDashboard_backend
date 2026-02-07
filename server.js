0f772fdc1391ad9c9595d8aff96fa807fce7df27
AleyJan <aliriayat507@gmail.com>
2026-02-07 23:22:07 +0500

    update in vercel.json

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Vite/React dev server
+      "https://user-dashboard-frontend-gray.vercel.app",
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
